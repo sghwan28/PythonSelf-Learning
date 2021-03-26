9e1c98567c9fe2d75e9e985684ec6e91e4229cf3
sghwan28 <sghwan28@gmail.com>
2021-03-26 08:53:45 +0800

    .

--- Exercise/checkio/OReilly.py
+++ Exercise/checkio/OReilly.py
@@ -35,8 +35,6 @@ def reverse_ascending(items):
     return f
 
 
-
-
 assert list(reverse_ascending([1, 2, 3, 4, 5])) == [5, 4, 3, 2, 1]
 assert list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])) == [10, 7, 5, 4, 8, 7, 2, 3, 1]
 assert list(reverse_ascending([5, 4, 3, 2, 1])) == [5, 4, 3, 2, 1]
--- Exercise/string/questions.py
+++
@@ -1,5 +0,0 @@
-def dis(c1: tuple, c2: tuple):
-    return ((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2) ** 0.5
-
-
-print(dis((2, 2), (0, 0)))
