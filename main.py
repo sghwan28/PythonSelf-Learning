eead3bcd680dc0c63d8b00390c7221fd6e53bdfc
sghwan28 <sghwan28@gmail.com>
2021-04-19 13:29:09 +0800

    one update

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-def compressString(S: str) -> str:
-    d = {}
-    for i in S:
-        d[i] = d.get(i, 0) + 1
+from typing import List
 
-    print(d)
-    res = ''
-    for key, value in d.items():
-        res += key
-        res += str(value)
+'''
+力扣 魔术索引
 
-    print(res)
+示例：
+输入：nums = [0, 2, 3, 4, 5]
+输出：0
+说明: 0下标的元素为0
+'''
 
-    return res if len(res) < len(S) else S
 
-compressString("aabcccccaa")
+def FindMagicIndex(nums: List[int]) -> int:
+    return next(iter(i for i, num in enumerate(nums) if i == num), -1)
+
+# iter 召唤一个迭代器
+# enumerate 排序器
+# next calls on the iterator, when no items is present, it throw an error or the default value
