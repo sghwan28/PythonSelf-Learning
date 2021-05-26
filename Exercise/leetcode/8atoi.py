759e3b7ee94ad1215f770728d2ed76cf95b2185d
sghwan28 <sghwan28@gmail.com>
2021-05-26 10:54:07 +0800

    one update

--- Exercise/leetcode/8atoi.py
+++ Exercise/leetcode/8atoi.py
@@ -1,3 +1,13 @@
+"""
+leetcode 中文站第8题
+
+自动机
+
+这道题也可以用正则，但是我不喜欢。
+
+"""
+
+
 INT_MAX = 2 ** 31 - 1
 INT_MIN = -2 ** 31
 
@@ -12,30 +22,34 @@ class Automaton:
             'signed': ['end', 'end', 'in_number', 'end'],
             'in_number': ['end', 'end', 'in_number', 'end'],
             'end': ['end', 'end', 'end', 'end'],
-        }
+        }  # 状态表
 
     def get_col(self, c):
-        if c.isspace():
+        if c.isspace():   # 为空格时，get函数会跳过运行
             return 0
-        if c == '+' or c == '-':
+        if c == '+' or c == '-':   # 为符号时，get函数执行状态转换
             return 1
-        if c.isdigit():
+        if c.isdigit():   # 为数字时， get函数执行运算
             return 2
-        return 3
+        return 3   # 跳过字母
 
     def get(self, c):
-        self.state = self.table[self.state][self.get_col(c)]
+        self.state = self.table[self.state][self.get_col(c)]   # state 转换
         if self.state == 'in_number':
-            self.ans = self.ans * 10 + int(c)
+            self.ans = self.ans * 10 + int(c)    # 10进制情况下，也可以根据不同进制进行调整
             self.ans = min(self.ans, INT_MAX) if self.sign == 1 else min(self.ans, -INT_MIN)
         elif self.state == 'signed':
             self.sign = 1 if c == '+' else -1
 
 
 class Solution:
-    def myAtoi(self, str: str) -> int:
+
+    def myAtoi(self, string: str) -> int:
         automaton = Automaton()
-        for c in str:
+        for c in string:
             automaton.get(c)
         return automaton.sign * automaton.ans
 
+
+c = Solution()
+c.myAtoi("   -1997w")
---
+++ Exercise/leetcode/CountAndSay.py
@@ -0,0 +1,39 @@
+"""
+力扣38题
+
+外观数列
+
+"""
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        pre = ''
+        cur = '1'
+
+        # 从第 2 项开始
+        for _ in range(1, n):
+            # 这里注意要将 cur 赋值给 pre
+            # 因为当前项，就是下一项的前一项。有点绕，尝试理解下
+            pre = cur
+            # 这里 cur 初始化为空，重新拼接
+            cur = ''
+            # 定义双指针 start，end
+            start = 0
+            end = 0
+            # 开始遍历前一项，开始描述
+            while end < len(pre):
+                # 统计重复元素的次数，出现不同元素时，停止
+                # 记录出现的次数，
+                while end < len(pre) and pre[start] == pre[end]:
+                    end += 1
+                # 元素出现次数与元素进行拼接
+                cur += str(end - start) + pre[start]
+                # 这里更新 start，开始记录下一个元素
+                start = end
+
+        return cur
+
+
+s = Solution()
+s.countAndSay(3)
